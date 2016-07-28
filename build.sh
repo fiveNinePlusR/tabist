521d0cf5ea5f4693b20b19682e7e7a26a531c67c
fiveNinePlusR <fiveNinePlusR@gmail.com>
2016-07-27 17:43:19 -0700

    add basic build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR"
+
+if [[ $DIR ]]
+then
+    rm -f ./**/.DS_Store
+    web-ext --source-dir "./tabist" --artifacts-dir "./web-ext-artifacts" build
+fi
+
