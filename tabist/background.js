edc5d4237ddad1b6bbb02e8280961ddbc46c6862
fiveNinePlusR <fiveNinePlusR@gmail.com>
2016-08-18 15:36:06 -0700

    Focus the tabist tab if there is one open. if there is not a Tabist tab open and the active tab is a "New Tab" then use that for the Tabist tab. Otherwise, create a new Tabist tab and focus to it.
    fixes #1 and fixes #2

--- tabist/background.js
+++ tabist/background.js
@@ -1,7 +1,35 @@
+var extURL = chrome.extension.getURL("tabs.html");
+
 function openMyPage() {
-   chrome.tabs.create({
-     "url": chrome.extension.getURL("tabs.html")
-   });
+	browser.windows.getCurrent({}).then(function(window) {
+		return browser.tabs.query({windowId: window.id});
+	}).then(function(tabs) {
+		//check for a tabist tab first
+		for(let tab of tabs) {
+			if(tab.title == "Tabist"){
+				//open that tab
+				chrome.tabs.update(tab.id, {active: true}); 
+				return;
+			}
+		}
+
+		//loop through again to see if there is an active new tab to replace
+		for(let tab of tabs) {
+			if(tab.title == "New Tab" && tab.active){
+				chrome.tabs.update(tab.id, {"url": extURL});
+				return;
+			}
+		}
+
+		//fallback to making a new tabist tab
+		chrome.tabs.create({
+			"url": extURL
+			//,"pinned": true // TODO: add pinned option
+		});
+		return;
+	}).catch(e => {
+		console.log(e.message);
+	});
 }
 
 chrome.browserAction.onClicked.addListener(openMyPage);
