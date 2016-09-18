82d1ab95a045f178d0a5b4f464dfc7661d10f7b8
fiveNinePlusR <fiveNinePlusR@gmail.com>
2016-09-18 12:34:08 -0700

    Add a keyboard shortcut to toggle tabist.
    Defaults to:
    
    Windows: Ctrl+Shift+E
    Mac: Command+Shift+E
    Linux: Ctrl+Shift+E

--- tabist/background.js
+++ tabist/background.js
@@ -1,34 +1,38 @@
 var extURL = chrome.extension.getURL("tabs.html");
 
-function openMyPage() {
-	chrome.windows.getCurrent({}, function(window) {
-		chrome.tabs.query({windowId: window.id}, function(tabs) {
-			//check for a tabist tab first
-			for(let tab of tabs) {
-				if(tab.title == "Tabist"){
+function toggleTabist() {
+  chrome.windows.getCurrent({}, function(window) {
+    chrome.tabs.query({windowId: window.id}, function(tabs) {
+    //check for a tabist tab first
+      for(let tab of tabs) {
+        if(tab.title == "Tabist"){
 					//open that tab
-					chrome.tabs.update(tab.id, {active: true}); 
-					return;
-				}
-			}
+          chrome.tabs.update(tab.id, {active: true});
+          return;
+        }
+      }
 
 			//loop through again to see if there is an active new tab to replace
-			for(let tab of tabs) {
-				if(tab.title == "New Tab" && tab.active){
-					chrome.tabs.update(tab.id, {"url": extURL});
-					return;
-				}
-			}
+      for(let tab of tabs) {
+        if(tab.title == "New Tab" && tab.active){
+          chrome.tabs.update(tab.id, {"url": extURL});
+          return;
+        }
+      }
 
-			//fallback to making a new tabist tab
-			chrome.tabs.create({
-				"url": extURL
-				//,"pinned": true // TODO: add pinned option
-			});
-			return;
-		})
-	});
+      //fallback to making a new tabist tab
+      chrome.tabs.create({
+        "url": extURL
+        //,"pinned": true // TODO: add pinned option
+      });
+      return;
+    });
+  });
 }
 
-chrome.browserAction.onClicked.addListener(openMyPage);
- 
+chrome.browserAction.onClicked.addListener(toggleTabist);
+
+// listen for the keyboard shortcut command
+chrome.commands.onCommand.addListener(function() {
+  toggleTabist();
+});
