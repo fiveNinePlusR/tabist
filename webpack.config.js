74696c17648ba6456a4f5a553dc64573a2616999
fiveNinePlusR <fiveNinePlusR@gmail.com>
2018-07-24 16:12:34 -0700

    make the sourcemap more useful

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const path = require('path');
 module.exports = {
   // mode: "production", // enable many optimizations for production builds
   mode: "development", // enabled useful tools for development
-  devtool: "nosources-source-map",
+  // devtool: "nosources-source-map", // enables stack traces but disables sources
+  devtool: "source-map", // enables stack traces but disables sources
 
   entry: {
     tabs: "./src/js/tabs.jsx",
